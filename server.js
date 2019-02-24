9a80f776fa136a75df1e83a3ef554b25591e215e
Avani Verma <avani.verma1998@gmail.com>
2019-02-24 17:47:26 +0530

    Adds shortening service for multiple URL and 404 handler

--- server.js
+++ server.js
@@ -2,38 +2,57 @@ const express = require('express');
 const app = express();
 const validUrl = require('valid-url');
 const shortid = require('shortid');
-const port = 6000 || process.env.port;
+const port = 5000||process.env.port;
 
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
- 
+
+app.get('/', function(req,res){
+    res.render('index', {});
+})
+
+app.set('view engine', 'hbs')
+app.use(express.static('views/images')); 
+
  
 
 // Creates a short URL for a single long URL
-// To-do : Apply it for array of URLs
-//         Link to a database
+// To-do : Link to a database
 //         Redirect to original URL from the short one
 //         Add search facility
-app.post('/shorten', function(req,res){
+app.post('/shorten', function(req,res)
+{
+    var x=req.body.url,arr=[],msg=[];
+    // It it's a single URL, make it an array
+    if(!(x.constructor === Array))  arr.push(x);
+    else  arr=x;
 
-    var url=req.body.url;
-    var msg ="";
-    // Checks validity of a URL
-    if (!validUrl.isUri(url))
-        msg="It doesn't look like a valid URL. Please try again !";
-    else
+    // Loop to check if all the URLs are valid, if yes shorten them
+    while(arr.length)
     {
+        url=arr.shift();
+        console.log(url)
+        // Checks validity of a URL
+        if (!validUrl.isUri(url))
+            msg.push(`${url} doesn't look like a valid URL.`);
+        else
+        {
         var obj={ 
-                    // Generate a short ID
+            // Generate a short ID
             short : shortid.generate(),
             original : url
         }
-        msg = `${url} shortened as ${obj.short}`;
+        msg.push(obj);
+        }
     }   
     res.send(msg);
 })
 
+// 404 Handler
+app.use(function(req,res){
+    res.render('404')
+})
 
 app.listen(port, function(){
     console.log(`Server listening on ${port}`)
